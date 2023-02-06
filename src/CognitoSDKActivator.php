18fba01a81ec384bf9b73a4397660ef0a7d05f0c
ccasanovas <ccasanovas.comex@gmail.com>
2023-02-06 20:23:05 -0300

    #fix(core): namespace

--- src/CognitoSDKActivator.php
+++ src/CognitoSDKActivator.php
@@ -7,13 +7,40 @@ use Composer\IO\IOInterface;
 use Composer\Plugin\PluginInterface;
 
 
-class CognitoSDKActivator implements PluginInterface
+class CognitoSDKActivator implements PluginInterface, EventSubscriberInterface
 {
+    protected $composer;
+    protected $io;
+
     public function activate(Composer $composer, IOInterface $io)
     {
-        $installer = new TemplateInstaller($io, $composer);
-        $composer->getInstallationManager()->addInstaller($installer);
+        $this->composer = $composer;
+        $this->io = $io;
     }
 
-}
+    public function deactivate(Composer $composer, IOInterface $io)
+    {
+    }
 
+    public function uninstall(Composer $composer, IOInterface $io)
+    {
+    }
+
+    public static function getSubscribedEvents()
+    {
+        return array(
+            PluginEvents::PRE_FILE_DOWNLOAD => array(
+                array('onPreFileDownload', 0)
+            ),
+        );
+    }
+
+    public function onPreFileDownload(PreFileDownloadEvent $event)
+    {
+        $protocol = parse_url($event->getProcessedUrl(), PHP_URL_SCHEME);
+
+        if ($protocol === 's3') {
+            // ...
+        }
+    }
+}
